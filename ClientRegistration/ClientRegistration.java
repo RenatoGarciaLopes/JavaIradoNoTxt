f5e8350c846538764de59489d0835e793be6aa1e
Gustavo10001 <gustavolimasossai@gmail.com>
2026-03-09 19:10:42 -0300

    feat: implement ClientRegistration to store client information in TXT file

--- ClientRegistration/ClientRegistration.java
+++ ClientRegistration/ClientRegistration.java
@@ -1,6 +1,9 @@
 package ClientRegistration;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 
 /**
  * Client registration: name, CPF and email to TXT file (no encryption).
@@ -8,12 +11,25 @@ import java.io.IOException;
  */
 public class ClientRegistration {
 
+  private static final String SEPARATOR = " | ";
+  private String filePath;
+
   public void setFilePath(String path) {
+    this.filePath = path;
   }
 
   /**
    * Saves client (name, CPF, email) to TXT file.
    */
   public void save(String name, String cpf, String email) throws IOException {
+
+    String line = cpf + SEPARATOR + email + SEPARATOR + name + System.lineSeparator();
+
+    Files.write(
+        Paths.get(filePath),
+        line.getBytes(),
+        StandardOpenOption.CREATE,
+        StandardOpenOption.APPEND
+    );
   }
 }
