74a9399862d0306a76584d0978fe1586e10e6aeb
Guilherme Brogio <guilhermebrogio.ps@gmail.com>
2026-03-09 21:32:28 -0300

    feat(ValidateLogin): add a class to test the implementation of validation login, and show a usage example

---
+++ ValidateLogin/TestValidateLogin.java
@@ -0,0 +1,8 @@
+package ValidateLogin;
+
+public class TestValidateLogin {
+  public static void main(String[] args) {
+    ValidateLogin validateLogin = new ValidateLogin("admin1@example.com", "suP3r-sTr0Ng-@-sEcrEt-pAs5W0rD");
+    System.out.println(validateLogin.isValid() ? "Login válido" : "Login inválido");
+  }
+}
